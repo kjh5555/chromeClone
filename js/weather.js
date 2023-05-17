2f3ebc23f707a37305d2bfbb2bbf5bb9a9387aa6
kjh5555 <69375870+kjh5555@users.noreply.github.com>
2023-05-17 20:45:46 +0900

    대충코딩

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,16 @@
+const APIKEY = "1f9727d1268500becf8a6fa2578eb20e";
+
 function onGeoOK(position){
     const lat =  position.coords.latitude;
     const lng =  position.coords.longitude
     console.log(`내 위치는 위도 : ${lat} 경도 :${lng}`)
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${APIKEY}&units=metric`
+    fetch(url).then(response => response.json()).then(data => {
+        const weatherCon = document.querySelector('#weather span:first-child');
+        const city = document.querySelector('#weather span:last-child');
+        city.innerText = data.name;
+        weatherCon.innerText = data.weather[0].main;
+    });
 }
 function onGeoErr(){
     alert("날씨정보를 찾을 수 없어요 ㅠㅠ")
